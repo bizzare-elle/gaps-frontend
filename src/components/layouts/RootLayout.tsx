8c01ad2e7eb98d9979b9a6d85f50909b8596bedc
Christian Rapsing <christianrapsing.primecorp@gmail.com>
2025-01-22 10:08:10 +0800

    sidebar

--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarInset } from "../ui/sidebar";
+import { SidebarProvider, SidebarInset, SidebarTrigger } from "../ui/sidebar";
 import SideBar from "../shared/SideBar";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { FaUser } from "react-icons/fa";
@@ -10,7 +10,10 @@ const RootLayout = () => {
       <SideBar />
       <SidebarInset>
         <main className="flex-1 bg-white inset-0">
-          <div className="py-5 px-8 flex justify-end">
+          <div className="py-5 pl-4  pr-8 flex justify-between">
+            <div>
+              <SidebarTrigger />
+            </div>
             <div className="flex items-center space-x-2">
               <div>
                 <Avatar className="flex items-center justify-center bg-gray-200">
--- src/components/shared/SideBar.tsx
+++ src/components/shared/SideBar.tsx
@@ -1,3 +1,4 @@
+import gaps from "../../assets/gaps.png";
 import {
   Sidebar,
   SidebarHeader,
@@ -8,8 +9,9 @@ import {
   SidebarMenuItem,
   SidebarContent,
   SidebarFooter,
+  useSidebar,
 } from "../ui/sidebar";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Avatar, AvatarImage } from "../ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -19,7 +21,6 @@ import {
 } from "../ui/dropdown-menu";
 import { FaUser } from "react-icons/fa";
 import { VscKebabVertical } from "react-icons/vsc";
-import logo from "../../assets/icon.png";
 import { IoBook } from "react-icons/io5";
 import { IoIosPaper } from "react-icons/io";
 import { Link, NavLink } from "react-router-dom";
@@ -27,6 +28,7 @@ import { Link, NavLink } from "react-router-dom";
 // import { FaBuildingColumns } from "react-icons/fa6";
 
 const SideBar = () => {
+  const { open } = useSidebar();
   const sidebarItems = [
     {
       title: "Courses",
@@ -44,15 +46,29 @@ const SideBar = () => {
     //   icon: <FaUsers />,
     // },
     // {
-    //   title: "Departments",
+    //   title: "D6epartments",
     //   icon: <FaBuildingColumns />,
     // },
   ];
 
   return (
-    <Sidebar className="border-none bg-white p-2">
-      <SidebarHeader className="bg-white p-5">
-        <img src={logo} alt="gaps-logo" className="w-[60%]" />
+    <Sidebar
+      defaultChecked={true}
+      collapsible="icon"
+      className={`border-none bg-white &{open ? 'p-2' : 'w-30'}`}
+    >
+      <SidebarHeader className="bg-white">
+        <div className="flex items-center gap-x-2">
+          <div>
+            <img src={gaps} alt="gaps-logo" className="w-[60px]" />
+          </div>
+          {open && (
+            <div className="flex flex-col leading-tight">
+              <span className="text-primary font-semibold text-[25px]">GAPS</span>
+              <span className="text-xs">Lorem ipsum, dolor </span>
+            </div>
+          )}
+        </div>
       </SidebarHeader>
       <SidebarContent className="bg-white">
         <SidebarGroup className="pt-10">
@@ -85,17 +101,13 @@ const SideBar = () => {
       <SidebarFooter>
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <div>
-              <Avatar className="flex items-center justify-center bg-gray-200">
-                <AvatarImage src="nfgernger" alt="avatar icon" />
-                <FaUser className="text-primary" size={20} />
-              </Avatar>
-            </div>
-            <div>
-              <span className="font-semibold">Dean</span>
-            </div>
+            <Avatar className={`flex items-center justify-center $(open ? '' : 'w-10')`}>
+              <AvatarImage src="nfgernger" alt="avatar icon" />
+              <FaUser className="text-primary absolute" size={open ? 20 : 15} />
+            </Avatar>
+            {open && <span className="font-semibold">Dean</span>}
           </div>
-          <div className="flex items-center">
+          {open && (
             <DropdownMenu>
               <DropdownMenuTrigger className="flex items-center justify-center">
                 <VscKebabVertical />
@@ -108,7 +120,7 @@ const SideBar = () => {
                 <DropdownMenuItem>Logout</DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
-          </div>
+          )}
         </div>
       </SidebarFooter>
     </Sidebar>
